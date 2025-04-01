5b3ee3ad06f0452cee95dde024bdaa857205ae3e
vito-lee3258 <vito_lee2026@outlook.com>
2025-04-01 10:59:52 +0800

    添加项目文件。

---
+++ HandEyeCalib/HandEyeCalib.cpp
@@ -0,0 +1,22 @@
+﻿// HandEyeCalib.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include "../HandEyeCalibration/pch.h"
+
+using namespace std;
+
+#pragma comment(lib, "HandEyeCalibration.lib")
+
+int main()
+{
+    IInterface* IF = IInterface::CreateInterface();
+
+    cout << IF->GetName() << endl;
+
+    IF->Init();
+
+    IF->Destroy();
+
+    return 0;
+}
---
+++ HandEyeCalibration/pch.h
@@ -0,0 +1,14 @@
+﻿#ifndef PCH_H
+#define PCH_H
+#define _CRT_SECURE_NO_WARNINGS
+#define FENGZHUANGCPP_API __declspec(dllexport) //导出
+//动态链接库
+class FENGZHUANGCPP_API IInterface
+{
+public:
+	static IInterface* CreateInterface();
+	virtual void Init() = 0;
+	virtual void Destroy() = 0;
+	virtual char* GetName() = 0;
+};
+#endif //PCH_H
